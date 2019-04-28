d14cdf807f874d1f524f1a3f56cb864f136fea12
Agreafel <csliw30@gmail.com>
2019-04-27 19:04:17 -0700

    Project Fixes (Name, Controls, Removed Broken Licht):  	modified:   LichtFiles/Licht.gd 	modified:   LichtFiles/icon.png 	modified:   LichtFiles/project.godot

--- LichtFiles/Licht.gd
+++ LichtFiles/Licht.gd
@@ -3,13 +3,13 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var lazerz = [1]
+#var lazerz = [1]
 var skelies = 0
 # Called when the node enters the scene tree for the first time.
 func testLight(testSpace, pos1, pos2, runs):
 	var result = testSpace.intersect_ray(pos1, pos2)
-	var positions = [pos1, pos2]
-	lazerz[runs] = positions
+	#var positions = [pos1, pos2]
+	#lazerz[runs] = positions
 	if(!result):
 		runs += 1
 		if(runs >= 100):
@@ -18,7 +18,7 @@ func testLight(testSpace, pos1, pos2, runs):
 	elif(result):
 		if(result.normal == -(pos1+pos2).normalized()):
 			return
-		testLight(testSpace, result.normal*128, result.position, runs)
+		#testLight(testSpace, result.normal*128, result.position, runs)
 
 func _ready():
 	skelies = 200
@@ -32,6 +32,7 @@ func _process(delta):
 		testLight(testSpace, self.position+(Vector2(1,0).normalized())*128, self.position+Vector2(64,0), 1)
 	
 func _draw():
-	for i in len(lazerz):
-		if(i):
-			draw_line(to_local(i), to_local(i), Color(1,1,1,1), 30, false)
+	pass
+	#for i in len(lazerz):
+		#if(i):
+			#draw_line(to_local(i), to_local(i), Color(1,1,1,1), 30, false)
